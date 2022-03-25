03b68f20ab28560e38e9f2ada19a9a63cd69aaf0
ardanq <ahmetardan.oz@gmail.com>
2022-03-25 18:37:19 +0300

    Added Razor View of Product and Sql Connection

---
+++ Data/AdminCRUDContext.cs
@@ -0,0 +1,12 @@
+﻿using AdminCRUD.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdminCRUD.Data
+{
+    public class AdminCRUDContext:DbContext
+    {
+        public AdminCRUDContext(DbContextOptions<AdminCRUDContext> options) : base(options) { }
+
+        public virtual DbSet<Product> Products { get; set; }
+    }
+}
