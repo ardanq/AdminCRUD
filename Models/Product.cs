eb74ef6a42578a726f76c1243a3660ff80827f1a
ardanq <ahmetardan.oz@gmail.com>
2022-03-25 17:57:11 +0300

    Add project files.

---
+++ Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+
+namespace AdminCRUD.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength (100)]
+        public string Name { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public int Price { get; set; }
+
+        [Required]
+        [DefaultValue("true")]
+        public bool Active { get; set; }
+
+
+
+    }
+}
