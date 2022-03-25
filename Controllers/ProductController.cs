c9a770e7d6852848881abfc0542c0bd97e9a0cae
ardanq <ahmetardan.oz@gmail.com>
2022-03-25 23:41:32 +0300

    Added create and update methods in ProductController.

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,5 +1,7 @@
 ﻿using AdminCRUD.Data;
+using AdminCRUD.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AdminCRUD.Controllers
 {
@@ -14,7 +16,49 @@ namespace AdminCRUD.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            List<Product> products = _context.Products.ToList();
+            return View(products);
         }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            Product prod = new Product();
+            return View(prod);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            _context.Add(product);
+            _context.SaveChanges();
+            return RedirectToAction("index");
+        }
+
+        public IActionResult Details(int Id)
+        {
+            Product prod = _context.Products.Where(x => x.Id == Id).FirstOrDefault();
+            return View(prod);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int Id)
+        {
+            Product prod = _context.Products.Where(x => x.Id == Id).FirstOrDefault();
+            return View(prod);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            _context.Attach(product);
+            _context.Entry(product).State = EntityState.Modified;
+            _context.SaveChanges();
+            return RedirectToAction("index");
+        }
+
+
+
+
     }
 }
