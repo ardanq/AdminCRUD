8def1096b80341bec3364bc68063d7491e59eb26
ardanq <ahmetardan.oz@gmail.com>
2022-03-25 19:41:49 +0300

    Added product controller constructor

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using AdminCRUD.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdminCRUD.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly AdminCRUDContext _context;
+
+        public ProductController(AdminCRUDContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
